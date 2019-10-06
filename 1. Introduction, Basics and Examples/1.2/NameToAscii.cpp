b31a87731f0ee3a7a528a1dfff4753145e879243
vanshianec <ivaniovov@abv.bg>
2019-10-07 01:11:45 +0300

    Init commit

---
+++ 1. Introduction, Basics and Examples/1.2/NameToAscii.cpp
@@ -0,0 +1,18 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string name;
+	cin >> name;
+	
+	for (int i = 0; i < name.length(); i++)
+	{
+		cout << (int) name[i];
+	}
+
+	cout << endl;
+}
