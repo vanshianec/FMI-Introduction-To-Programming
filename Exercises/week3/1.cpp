5b780bb7ddb600e2a2be6b3dd3ca666ece591710
vanshianec <ivaniovov@abv.bg>
2019-10-17 08:53:19 +0300

    Init commit

--- Exercises/week3/1.cpp
+++ Exercises/week3/1.cpp
@@ -1,29 +1,29 @@
 #include <iostream>
- 
+
 using namespace std;
- 
-int main ()
+
+int main()
 {
-    int n;
- 
-    cin >> n;
- 
-    for (int z = 1; z <= n; z++)
-    {
-        for (int i = 1; i <= 2 * z; i++)
-        {
+	int n;
+
+	cin >> n;
+
+	for (int z = 1; z <= n; z++)
+	{
+		for (int i = 1; i <= 2 * z; i++)
+		{
 			for (int k = 0; k < 2 * n - i; k++)
-            {
-                cout << " ";
-            }
- 
-            for (int j = 1; j <= i; j++)
-            {            
-                cout << "* ";
-            }
- 
-            cout << endl;
-        }
- 
-    }
+			{
+				cout << " ";
+			}
+
+			for (int j = 1; j <= i; j++)
+			{
+				cout << "* ";
+			}
+
+			cout << endl;
+		}
+
+	}
 }
