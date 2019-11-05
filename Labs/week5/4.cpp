94e1967b8e2645c2cd4c12eff2e6b78cf0066d35
vanshianec <ivaniovov@abv.bg>
2019-11-05 23:07:25 +0200

    Init commit

--- Labs/week5/4.cpp
+++ Labs/week5/4.cpp
@@ -1,30 +1,33 @@
+
+#include "pch.h"
 #include <iostream>
 #include <cmath>
- 
+
 using namespace std;
- 
- 
- 
-int main ()
+
+
+
+int main()
 {
-    int n, num;
-    cin >> n;
-    double result, s, s_numerator, result_numerator;
- 
-    for (int i = 0; i < n; i++)
-    {
-        cin >> num;
-        s_numerator += num;
-        result_numerator += pow(num , 2);
-    }
- 
-    s = s_numerator / n;
- 
-    result = sqrt((n * pow(s, 2) + result_numerator - 2 * s * (s_numerator))/ n - 1);
- 
- 
-    cout << result << endl;
- 
-   
-    return 0;
+	int n, num;
+	cin >> n;
+	double result, s, s_numerator = 0, result_numerator = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> num;
+		s_numerator += num;
+		result_numerator += pow(num, 2);
+	}
+
+	s = s_numerator / n;
+
+	result = sqrt((n * pow(s, 2) + result_numerator - 2 * s * (s_numerator)) / n - 1);
+
+
+	cout << result << endl;
+
+
+	return 0;
 }
+
