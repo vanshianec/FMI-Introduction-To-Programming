120f8782613eca13a8a05de584eee6addd8cb95d
vanshianec <ivaniovov@abv.bg>
2019-11-06 00:23:11 +0200

    Init commit

---
+++ Homework/13. Recursion/5.4/RecursiveMultiplicationWithSkips.cpp
@@ -0,0 +1,22 @@
+﻿
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int multiplication(int n, int k)
+{
+	if (n <= 1)
+	{
+		return 1;
+	}
+	
+	return n * multiplication(n - k, k);
+}
+
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+	cout << multiplication(n, k);
+}
