e1be14e0afd11f928d458fe7f45b9acc46a27632
vanshianec <ivaniovov@abv.bg>
2019-11-09 13:59:31 +0200

    Init commit

---
+++ Homework/13. Recursion/5.5/RecursiveCountDigitsFromKthNumericSystem.cpp
@@ -0,0 +1,24 @@
+﻿
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int sumOfKthNumericSystemDigits(int num, int k)
+{
+	if (num == 0)
+	{
+		return 0;
+	}
+
+	return num % k + sumOfKthNumericSystemDigits(num / k, k);
+}
+
+int main()
+{
+	int num, k;
+	cin >> num >> k;
+
+	cout << sumOfKthNumericSystemDigits(num, k);
+
+}
